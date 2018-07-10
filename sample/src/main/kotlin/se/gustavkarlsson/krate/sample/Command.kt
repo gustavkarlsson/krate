8d0f2ec7bea9ac6f5683ee5e27874c532971d1c3
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-07-10 22:57:51 +0200

    Fixed linting issues

--- sample/src/main/kotlin/se/gustavkarlsson/krate/sample/Command.kt
+++ sample/src/main/kotlin/se/gustavkarlsson/krate/sample/Command.kt
@@ -4,5 +4,5 @@ sealed class Command {
     object GetNotes : Command()
     data class AddNote(val text: String, val important: Boolean = false) : Command()
     data class DeleteNote(val note: Note) : Command()
-    data class AcknowledgeError(val error: String): Command()
+    data class AcknowledgeError(val error: String) : Command()
 }
--- sample/src/main/kotlin/se/gustavkarlsson/krate/sample/Result.kt
+++ sample/src/main/kotlin/se/gustavkarlsson/krate/sample/Result.kt
@@ -1,6 +1,5 @@
 package se.gustavkarlsson.krate.sample
 
-
 sealed class Result {
     data class NotesAdded(val notes: List<Note>) : Result()
     data class NoteRemoved(val id: Long) : Result()
