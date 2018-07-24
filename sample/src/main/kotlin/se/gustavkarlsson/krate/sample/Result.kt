c9224f08277dfa4e93631de2ba2d3ba7835dd588
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-07-24 16:46:31 +0200

    Removed old sample

--- sample/src/main/kotlin/se/gustavkarlsson/krate/sample/Result.kt
+++
@@ -1,8 +0,0 @@
-package se.gustavkarlsson.krate.sample
-
-sealed class Result {
-    data class NotesAdded(val notes: List<Note>) : Result()
-    data class NoteRemoved(val id: Long) : Result()
-    data class ErrorEncountered(val throwable: Throwable) : Result()
-    data class ErrorAcknowledged(val error: String) : Result()
-}
