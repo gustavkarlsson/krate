df16995d9e9b498a396b7ef76d978f3c4c0d28bc
Gustav Karlsson <gustav.karlsson@gmail.com>
2019-02-01 20:44:41 +0100

    Trying to reactify VCR

--- plugins/vcr/src/main/kotlin/se/gustavkarlsson/krate/vcr/Vcr.kt
+++ plugins/vcr/src/main/kotlin/se/gustavkarlsson/krate/vcr/Vcr.kt
@@ -12,9 +12,17 @@ import se.gustavkarlsson.krate.core.Interceptor
 import se.gustavkarlsson.krate.core.dsl.StorePlugin
 import java.util.concurrent.TimeUnit
 
+private sealed class Command<TapeId> {
+    object Stop : Command<Nothing>()
+    data class Record<TapeId>(val tapeId: TapeId, val startTime: Long) : Command<TapeId>()
+    data class Play<TapeId>(val tapeId: TapeId) : Command<TapeId>()
+    data class Erase<TapeId>(val tapeId: TapeId) : Command<TapeId>()
+}
+
 abstract class Vcr<State : Any, TapeId>(
     private val currentTimeMillis: () -> Long = System::currentTimeMillis
 ) : StorePlugin<State, Any, Any> {
+    private val commands = PublishSubject.create<Command<TapeId>>().serialize()
     private val playingSubject = PublishSubject.create<State>()
     private val recordingSubject = BehaviorSubject.create<State>()
 
@@ -31,9 +39,20 @@ abstract class Vcr<State : Any, TapeId>(
     override fun changeStateInterceptors(interceptors: List<Interceptor<State>>): List<Interceptor<State>> =
         interceptors + Record() + Play()
 
-    @Synchronized
-    fun record(tapeId: TapeId) {
-        stop()
+    init {
+        commands
+            .switchMapCompletable {
+                when (it) {
+                    is Command.Stop -> Completable.complete()
+                    is Command.Record -> doRecord(it.tapeId)
+                    is Command.Play -> doPlay(it.tapeId)
+                    is Command.Erase -> doErase(it.tapeId)
+                }
+            }
+            .subscribe()
+    }
+
+    private fun doRecord(tapeId: TapeId): Completable {
         startTime = currentTimeMillis()
         recording = startRecording(tapeId)
             .flatMapCompletable { recording ->
@@ -48,28 +67,15 @@ abstract class Vcr<State : Any, TapeId>(
             .subscribe()
     }
 
-    @Synchronized
-    fun play(tapeId: TapeId) {
-        stop()
+    fun doPlay(tapeId: TapeId): Completable {
         playback = startPlaying(tapeId)
             .delay { Flowable.timer(it.timestamp, TimeUnit.MILLISECONDS) }
             .map { it.state }
             .subscribe(playingSubject::onNext)
     }
 
-    @Synchronized
-    fun erase(tapeId: TapeId) {
-        stop()
+    fun doErase(tapeId: TapeId): Completable {
         eraseTape(tapeId)
-    }
-
-    @Synchronized
-    fun stop() {
-        recording?.dispose()
-        recording = null
-        playback?.dispose()
-        playback = null
-        startTime = 0
     }
 
     val isRecording: Boolean
